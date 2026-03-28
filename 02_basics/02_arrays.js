f03cd700c3198b16ec88576d657df9ce1110cf97
Deepak Kumar Ram <35396750+er-deepak-ram@users.noreply.github.com>
2026-03-28 09:26:17 +0530

    Comments added for 02_arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -18,11 +18,22 @@ console.log("Using spread operator: ", allNewHeros); // Output: [ 'Thor', 'Ironm
 
 const complicatedArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 const flattenedArray = complicatedArray.flat(Infinity); // flattens the array up to Infinity levels deep
-console.log(flattenedArray);
+console.log(flattenedArray); // [1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5]
+/**
+ * the Infinity here is a special built‑in constant in JavaScript. It represents a value larger than any finite number.
+ * When used as the argument to .flat(), it means:
+ *      - "Flatten this array to any depth necessary."
+ *      - Instead of specifying a number like 1 (one level deep) or 2 (two levels deep), Infinity tells JavaScript to keep 
+ *        flattening until there are no nested arrays left.
+ */
 
 console.log(Array.isArray("Deeppak")); // false
 console.log(Array.from("Deeppak")); // [ 'D', 'e', 'e', 'p', 'p', 'a', 'k' ]
-console.log(Array.from({name: "Deeppak"})); // [] Interesting case
+console.log(Array.from({name: "Deeppak"})); // [] Passing object to Array.from() method -> Interesting case for interviews
+/**
+ * If we pass an object to the Array.from() method, it will not be able to convert it into an array, that's why it will return an empty array
+ * To be able to convert an object into an array we have to tell if we want to consider keys into array or values into array
+ */
 
 let score1 = 100
 let score2 = 200
